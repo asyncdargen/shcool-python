cc5000795a54dbcea9fc145b7fd97a67945cbb09
AndrewVasilev <71066575+asyncdargen@users.noreply.github.com>
2022-09-16 08:31:22 +0000

    add two lesson tasks

---
+++ lesson-2/task-1.py
@@ -0,0 +1,12 @@
+a, b, c = map(int, input().split())
+
+mx = max(a, b, c)
+mn = min(a, b, c)
+md = a + b + c - mx - mn
+
+if mx ** 2 > mn ** 2 + md ** 2:
+    print("Треугольник тупоугольный")
+elif mx ** 2 == mn ** 2 + md ** 2:
+    print("Треугольник прямоугольный")
+else:
+    print("Треугольник остроуголный")
